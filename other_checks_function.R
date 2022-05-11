a8ff1a80d4706f10189fcf39423c87b3533aa790
atmathias <atmathias@gmail.com>
2022-05-11 20:09:02 +0300

    check:arranging data, adding choices and joining survey to parent question

--- other_checks_function.R
+++ other_checks_function.R
@@ -2,7 +2,7 @@
 
 # Extract other specify function ------------------------------------------
 
-other_data <- function(input_dfa_tool_data, input_survey, input_choices) {
+other_data_extract <- function(input_dfa_tool_data, input_survey, input_choices) {
   
   # add and rename some columns
   dfa_data <- input_dfa_tool_data %>% 
@@ -35,5 +35,31 @@ other_data <- function(input_dfa_tool_data, input_survey, input_choices) {
     dfa_other_response_data <- rbind(dfa_other_response_data, dfa_filtered_data)
   }
   
+  # arrange data
+  dfa_arranged_data <- dfa_other_response_data %>% 
+    arrange(start_date, uuid)
+    
+  # get choices to add to the _other responses extracted
+  dfa_grouped_choices <- input_choices %>% 
+    group_by(list_name)
+    summarize(choice_options = paste(name, collapse = " : ")) %>% 
+    arrange(list_name)  
+    
+    
+  # extract parent question and join survey for existing list_name
+    dfa_data_parent_qns <- dfa_arranged_data %>% 
+    left_join(input_survey %>% select(name, type), by = c("parent_qn"="name")) %>% 
+    separate(col = type, into = c(""))
+      
   
-}
+    
+    
+    
+    
+    
+    
+    
+
+ }
+
+
